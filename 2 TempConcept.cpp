b2ccba179bc632794842f9718096534b4efae49c
MemoonaMobeen <memoonamobeen618@gmail.com>
2023-03-05 17:44:35 +0500

    Daring to code

---
+++ 2 TempConcept.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m=2, o=7, n=3, a=8, temp;
+	
+	cout<<"Values before swappping: "<<"\n";
+	cout<<m<<", "<<o<<", "<<n<<", "<<a<<"\n";
+	
+	temp= m;
+	m=o;
+	o=n;
+	n=a;
+	a=temp;  
+	
+		cout<<" Values after swappping: "<<"\n";
+	cout<<m<<", "<<o<<", "<<n<<", "<<a;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 return 0;
+}
---
+++ ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ char mona;
+ cout << "Enter a character: ";
+ cin >> mona;
+ cout << "ASCII Value of " << mona << " is " << int(mona);
+ return 0;
+}
